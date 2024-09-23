ce673f60897cd31b04e39bbdf977bc24f3a3649a
unknown <tanechka.kovpak@yandex.ru>
2024-09-23 14:40:34 +0300

    1

--- social-link/index.tsx
+++ social-link/index.tsx
@@ -9,30 +9,30 @@ import instagramIcon from '../../../assets/icons/social_instagram_light_grey.svg
 import facebookIcon from '../../../assets/icons/social_facebook_light_grey.svg';
 import inIcon from '../../../assets/icons/social_in_light_grey.svg';
 
-const socialLinkIcon: { [key in SocialLinkTypes]: string } = {
-	[SocialLinkTypes.telegram]: telegramIcon,
-	[SocialLinkTypes.vk]: vkIcon,
-	[SocialLinkTypes.ok]: okIcon,
-	[SocialLinkTypes.youtube]: youtubeIcon,
-	[SocialLinkTypes.instagram]: instagramIcon,
-	[SocialLinkTypes.facebook]: facebookIcon,
-	[SocialLinkTypes.in]: inIcon,
+const socialLinkIcon: Record<SocialLinkTypes, string> = {
+  [SocialLinkTypes.telegram]: telegramIcon,
+  [SocialLinkTypes.vk]: vkIcon,
+  [SocialLinkTypes.ok]: okIcon,
+  [SocialLinkTypes.youtube]: youtubeIcon,
+  [SocialLinkTypes.instagram]: instagramIcon,
+  [SocialLinkTypes.facebook]: facebookIcon,
+  [SocialLinkTypes.in]: inIcon,
 };
 
 const SocialLink: React.FC<ISocialLink> = ({ name, link, width, children }) => {
-	const styleWidth =
-		width === SocialLinkSize.small ? `${style.small}` : `${style.medium}`;
+  const styleWidth =
+    width === SocialLinkSize.small ? `${style.small}` : `${style.medium}`;
 
-	return (
-		<a href={link} className={`${style.link}`} target="blank">
-			<img
-				className={`${styleWidth} ${style.icon}`}
-				src={socialLinkIcon[name] as string}
-				alt={`логотип ${name}`}
-			/>
-			{children}
-		</a>
-	);
+  return (
+    <a href={link} className={`${style.link}`} target="blank">
+      <img
+        className={`${styleWidth} ${style.icon}`}
+        src={socialLinkIcon[name as SocialLinkTypes]}
+        alt={`логотип ${name}`}
+      />
+      {children}
+    </a>
+  );
 };
 
 export default SocialLink;
